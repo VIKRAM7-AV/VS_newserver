c314156f851d487ff5d5bc1fc8f3bdb7d02c9d1a
VIKRAM <131445257+VIKRAM7-AV@users.noreply.github.com>
2025-07-03 13:54:31 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,7 +7,7 @@ res.cookie("token", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", 
-    sameSite: "lax",
+    sameSite: "none",
 });
 
 }
